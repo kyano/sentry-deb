eb0d77b50a12344d980adba6502b37ff515eaec4
Olivier Chédru <ochedru@dhatim.com>
2016-05-16 15:50:34 +0200

    Bump version

--- pkg/setup.py
+++ pkg/setup.py
@@ -7,7 +7,7 @@ if __name__ == '__main__' and sys.argv[1] == 'test':
 
 setup(
     name='sentry',
-    version='7.7.0',
+    version='8.4.0',
     author='Raphaël Berbain',
     author_email='rberbain@dhatim.com',
     packages=[],
